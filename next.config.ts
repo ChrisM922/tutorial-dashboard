d19f036d2370d34efcb7fb25346947b28c8337ac
ChrisM922 <112943995+ChrisM922@users.noreply.github.com>
2025-01-30 14:16:02 +0100

    Refactor next.config.ts to remove local image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    localPatterns: [{ pathname: '/public/*' }],
-  },
   /* config options here */
   experimental: {
     ppr: 'incremental',
